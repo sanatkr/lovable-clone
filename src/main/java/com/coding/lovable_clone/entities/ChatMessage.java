53aca4dd381388e850e0fbaf039403e41e60b0e6
sanat <sanatrathore1214@gmail.com>
2026-02-06 02:00:21 +0530

    Chat and Usage Entity Created.

---
+++ src/main/java/com/coding/lovable_clone/entities/ChatMessage.java
@@ -0,0 +1,29 @@
+package com.coding.lovable_clone.entities;
+
+
+import com.coding.lovable_clone.enums.MessageRole;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ChatMessage {
+
+    Long id;
+    ChatSession chatSession;
+
+    String content;
+
+    MessageRole role;
+
+    String toolCalls; // JSON Array of Tools Called
+
+    Integer tokensUsed;
+
+    Instant createdAt;
+}
