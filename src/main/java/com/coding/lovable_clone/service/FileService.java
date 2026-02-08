2534f391338f497b3a37645c8837195464f03911
sanat <sanatrathore1214@gmail.com>
2026-02-08 20:55:23 +0530

    File Controller,Service and DTO created.

---
+++ src/main/java/com/coding/lovable_clone/service/FileService.java
@@ -0,0 +1,13 @@
+package com.coding.lovable_clone.service;
+
+import com.coding.lovable_clone.dto.project.FileContentResponse;
+import com.coding.lovable_clone.dto.project.FileNode;
+
+import java.util.List;
+
+public interface FileService {
+
+    List<FileNode> getFileTree(Long projectId, Long userId);
+
+    FileContentResponse getFileContent(Long projectId, String path, Long userId);
+}
