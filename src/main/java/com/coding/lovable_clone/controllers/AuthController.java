1e29774eb995e05945e1eae48441826f807952d6
sanat <sanatrathore1214@gmail.com>
2026-02-08 23:26:54 +0530

    AuthController Modified-1.

--- src/main/java/com/coding/lovable_clone/controllers/AuthController.java
+++ src/main/java/com/coding/lovable_clone/controllers/AuthController.java
@@ -6,28 +6,28 @@ import com.coding.lovable_clone.dto.auth.SignupRequest;
 import com.coding.lovable_clone.dto.auth.UserProfileResponse;
 import com.coding.lovable_clone.service.AuthService;
 import com.coding.lovable_clone.service.UserService;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/auth")
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class AuthController {
 
-    private AuthService authService;
-    private UserService userService;
+    AuthService authService;
+    UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<AuthResponse> signup(SignupRequest request) {
+    public ResponseEntity<AuthResponse> signup(@RequestBody SignupRequest request) {
         return ResponseEntity.ok(authService.signup(request));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<AuthResponse> login(LoginRequest request) {
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
 
