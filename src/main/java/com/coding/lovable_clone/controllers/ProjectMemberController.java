1110a278735891718f20ca1b1dfa3da2a4204e9d
sanat <sanatrathore1214@gmail.com>
2026-02-09 21:00:04 +0530

    ProjectMemberController &  ProjectMemberService Modified-1.

--- src/main/java/com/coding/lovable_clone/controllers/ProjectMemberController.java
+++ src/main/java/com/coding/lovable_clone/controllers/ProjectMemberController.java
@@ -2,9 +2,12 @@ package com.coding.lovable_clone.controllers;
 
 import com.coding.lovable_clone.dto.member.InviteMemberRequest;
 import com.coding.lovable_clone.dto.member.MemberResponse;
+import com.coding.lovable_clone.dto.member.UpdateMemberRoleRequest;
 import com.coding.lovable_clone.entities.ProjectMember;
 import com.coding.lovable_clone.service.ProjectMemberService;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,12 +17,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/projects/{projectId}/members")
 @RequiredArgsConstructor
+@FieldDefaults(makeFinal = true,level = AccessLevel.PRIVATE)
 public class ProjectMemberController {
 
-    private final ProjectMemberService projectMemberService;
+    ProjectMemberService projectMemberService;
 
     @GetMapping
-    public ResponseEntity<List<ProjectMember>> getProjectMembers(@PathVariable Long projectId) {
+    public ResponseEntity<List<MemberResponse>> getProjectMembers(@PathVariable Long projectId) {
         Long userId = 1L;
         return ResponseEntity.ok(projectMemberService.getProjectMembers(projectId, userId));
     }
@@ -39,19 +43,20 @@ public class ProjectMemberController {
     public ResponseEntity<MemberResponse> updateMemberRole(
             @PathVariable Long projectId,
             @PathVariable Long memberId,
-            @RequestBody InviteMemberRequest request
+            @RequestBody UpdateMemberRoleRequest request
     ) {
         Long userId = 1L;
         return ResponseEntity.ok(projectMemberService.updateMemberRole(projectId, memberId, request, userId));
     }
 
     @DeleteMapping("/{memberId}")
-    public ResponseEntity<MemberResponse> updateMemberRole(
+    public ResponseEntity<MemberResponse> deleteMember(
             @PathVariable Long projectId,
             @PathVariable Long memberId
     ) {
         Long userId = 1L;
         return ResponseEntity.ok(projectMemberService.deleteProjectMember(projectId, memberId, userId));
     }
+
 
 }
--- src/main/java/com/coding/lovable_clone/service/ProjectMemberService.java
+++ src/main/java/com/coding/lovable_clone/service/ProjectMemberService.java
@@ -2,17 +2,18 @@ package com.coding.lovable_clone.service;
 
 import com.coding.lovable_clone.dto.member.InviteMemberRequest;
 import com.coding.lovable_clone.dto.member.MemberResponse;
+import com.coding.lovable_clone.dto.member.UpdateMemberRoleRequest;
 import com.coding.lovable_clone.entities.ProjectMember;
 
 import java.util.List;
 
 public interface ProjectMemberService {
 
-    List<ProjectMember> getProjectMembers(Long projectId, Long userId);
+    List<MemberResponse> getProjectMembers(Long projectId, Long userId);
 
     MemberResponse inviteMember(Long projectId, InviteMemberRequest request, Long userId);
 
-    MemberResponse updateMemberRole(Long projectId, Long memberId, InviteMemberRequest request, Long userId);
+    MemberResponse updateMemberRole(Long projectId, Long memberId, UpdateMemberRoleRequest request, Long userId);
 
     MemberResponse deleteProjectMember(Long projectId, Long memberId, Long userId);
 }
