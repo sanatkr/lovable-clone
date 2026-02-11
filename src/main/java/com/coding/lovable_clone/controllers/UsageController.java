5f783559c3ee758a83fa2f70662e0e679a7ee73f
sanat <sanatrathore1214@gmail.com>
2026-02-11 18:12:20 +0530

    Usage Controller modified-1.

--- src/main/java/com/coding/lovable_clone/controllers/UsageController.java
+++ src/main/java/com/coding/lovable_clone/controllers/UsageController.java
@@ -3,7 +3,9 @@ package com.coding.lovable_clone.controllers;
 import com.coding.lovable_clone.dto.subscription.PlanLimitsResponse;
 import com.coding.lovable_clone.dto.subscription.UsageTodayResponse;
 import com.coding.lovable_clone.service.UsageService;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/usage")
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class UsageController {
 
-    private final UsageService usageService;
+    UsageService usageService;
 
     @GetMapping("/today")
     public ResponseEntity<UsageTodayResponse> getTodayUsage() {
