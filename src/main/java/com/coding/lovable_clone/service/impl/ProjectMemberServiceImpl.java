7bee7aa35db00e71750af9aea689933d76f8a050
sanat <sanatrathore1214@gmail.com>
2026-02-14 21:28:05 +0530

    ProjectMemberServic Impl class created.

---
+++ src/main/java/com/coding/lovable_clone/service/impl/ProjectMemberServiceImpl.java
@@ -0,0 +1,30 @@
+package com.coding.lovable_clone.service.impl;
+
+import com.coding.lovable_clone.dto.member.InviteMemberRequest;
+import com.coding.lovable_clone.dto.member.MemberResponse;
+import com.coding.lovable_clone.dto.member.UpdateMemberRoleRequest;
+import com.coding.lovable_clone.service.ProjectMemberService;
+
+import java.util.List;
+
+public class ProjectMemberServiceImpl implements ProjectMemberService {
+    @Override
+    public List<MemberResponse> getProjectMembers(Long projectId, Long userId) {
+        return List.of();
+    }
+
+    @Override
+    public MemberResponse inviteMember(Long projectId, InviteMemberRequest request, Long userId) {
+        return null;
+    }
+
+    @Override
+    public MemberResponse updateMemberRole(Long projectId, Long memberId, UpdateMemberRoleRequest request, Long userId) {
+        return null;
+    }
+
+    @Override
+    public MemberResponse deleteProjectMember(Long projectId, Long memberId, Long userId) {
+        return null;
+    }
+}
