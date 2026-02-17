0e80f031fb779d756716c1685dbf097d717e23ac
sanat <sanatrathore1214@gmail.com>
2026-02-17 22:29:53 +0530

    PlanService implementation class created.

---
+++ src/main/java/com/coding/lovable_clone/service/impl/PlanServiceImpl.java
@@ -0,0 +1,16 @@
+package com.coding.lovable_clone.service.impl;
+
+import com.coding.lovable_clone.dto.subscription.PlanResponse;
+import com.coding.lovable_clone.service.PlanService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PlanServiceImpl implements PlanService {
+
+    @Override
+    public List<PlanResponse> getAllActivePlans() {
+        return List.of();
+    }
+}
