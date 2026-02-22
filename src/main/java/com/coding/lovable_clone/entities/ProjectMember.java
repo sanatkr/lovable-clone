e1de33819023c440544ad67efb8deec700986ab4
sanat <sanatrathore1214@gmail.com>
2026-02-23 00:43:21 +0530

    ProjectMember and ProjectMemberId class updated-1.

--- src/main/java/com/coding/lovable_clone/entities/ProjectMember.java
+++ src/main/java/com/coding/lovable_clone/entities/ProjectMember.java
@@ -1,19 +1,38 @@
 package com.coding.lovable_clone.entities;
 
 import com.coding.lovable_clone.enums.ProjectRole;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 import java.time.Instant;
 
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "project_members")
 public class ProjectMember {
 
+    @EmbeddedId
     ProjectMemberId id;
 
+    @ManyToOne
+    @MapsId("projectId")
     Project project;
 
+    @ManyToOne
+    @MapsId("userId")
     User user;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     ProjectRole projectRole;
 
     Instant invitedAt;
+
     Instant acceptedAt;
 }
