9861645187bb1b74f56a87e69e6aa32ea6e82536
sanat <sanatrathore1214@gmail.com>
2026-03-03 23:10:12 +0530

    ProjectRole enum modified using ProjectPermission enum.

--- src/main/java/com/coding/lovable_clone/enums/ProjectRole.java
+++ src/main/java/com/coding/lovable_clone/enums/ProjectRole.java
@@ -1,7 +1,24 @@
 package com.coding.lovable_clone.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Set;
+
+import static com.coding.lovable_clone.enums.ProjectPermission.*;
+
+@RequiredArgsConstructor
+@Getter
 public enum ProjectRole {
 
-    EDITOR, VIEWER,OWNER
+    EDITOR(VIEW, EDIT, DELETE, VIEW_MEMBERS),
+    VIEWER(Set.of(VIEW, VIEW_MEMBERS)),
+    OWNER(Set.of(VIEW, EDIT, DELETE, MANAGE_MEMBERS, VIEW_MEMBERS));
+
+    ProjectRole(ProjectPermission... permissions) {
+        this.permissions = Set.of(permissions);
+    }
+
+    private final Set<ProjectPermission> permissions;
 
 }
