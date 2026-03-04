bd9eb89c89b0173deb4a3687eff2a228208534f7
sanat <sanatrathore1214@gmail.com>
2026-03-04 18:06:24 +0530

    Queries modified in ProjectRepository interface.

--- src/main/java/com/coding/lovable_clone/repository/ProjectRepository.java
+++ src/main/java/com/coding/lovable_clone/repository/ProjectRepository.java
@@ -1,23 +1,60 @@
 package com.coding.lovable_clone.repository;
 
 import com.coding.lovable_clone.entities.Project;
+import com.coding.lovable_clone.enums.ProjectRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
     @Query("""
-            SELECT p FROM Project p
-            WHERE p.deletedAt IS NULL
-            AND p.owner.id = :userId
+            SELECT p as project, pm.projectRole as role
+            FROM Project p
+            JOIN ProjectMember pm ON pm.project.id = p.id
+            WHERE pm.user.id = :userId
+              AND p.deletedAt IS NULL
             ORDER BY p.updatedAt DESC
-            """
-    )
-    List<Project> findAllAccessibleByUser(@Param("userId") Long userId);
+            """)
+    List<ProjectWithRole> findAllAccessibleByUser(@Param("userId") Long userId);
+
+
+    @Query("""
+            SELECT p FROM Project p
+            WHERE p.id = :projectId
+                AND p.deletedAt IS NULL
+                AND EXISTS (
+                    SELECT 1 FROM ProjectMember pm
+                    WHERE pm.id.userId = :userId
+                    AND pm.id.projectId = :projectId
+                )
+            """)
+    Optional<Project> findAccessibleProjectById(@Param("projectId") Long projectId,
+                                                @Param("userId") Long userId);
+
+
+    @Query("""
+            SELECT p as project, pm.projectRole as role
+            FROM Project p
+            JOIN ProjectMember pm ON pm.project.id = p.id
+            WHERE p.id = :projectId
+              AND pm.user.id = :userId
+              AND p.deletedAt IS NULL
+            """)
+    Optional<ProjectWithRole> findAccessibleProjectByIdWithRole(@Param("projectId") Long projectId,
+                                                                @Param("userId") Long userId);
+
+
+    interface ProjectWithRole {
+        Project getProject();
+        ProjectRole getRole();
+    }
+
+
 
 }
