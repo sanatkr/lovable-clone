099ffd4b8ae265b7bd107434bb8583df4e51fe0f
sanat <sanatrathore1214@gmail.com>
2026-03-07 23:38:34 +0530

    ProjectMemberRepository created in the repository package.

---
+++ src/main/java/com/coding/lovable_clone/repository/ProjectMemberRepository.java
@@ -0,0 +1,30 @@
+package com.coding.lovable_clone.repository;
+
+import com.coding.lovable_clone.entities.ProjectMember;
+import com.coding.lovable_clone.entities.ProjectMemberId;
+import com.coding.lovable_clone.enums.ProjectRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProjectMemberRepository extends JpaRepository<ProjectMember, ProjectMemberId> {
+
+    List<ProjectMember> findByIdProjectId(Long projectId);
+
+    @Query("""
+            SELECT pm.projectRole FROM ProjectMember pm
+            WHERE pm.id.projectId = :projectId AND pm.id.userId = :userId
+            """)
+    Optional<ProjectRole> findRoleByProjectIdAndUserId(@Param("projectId") Long projectId,
+                                                       @Param("userId") Long userId);
+
+
+    @Query("""
+            SELECT COUNT(pm) FROM ProjectMember pm
+            WHERE pm.id.userId = :userId AND pm.projectRole = 'OWNER'
+            """)
+    int countProjectOwnedByUser(@Param("userId") Long userId);
+}
