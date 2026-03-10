55a3b4382b48f63c47aee7dd24b483c10ea1203c
sanat <sanatrathore1214@gmail.com>
2026-03-10 22:07:18 +0530

    toProjectSummaryResponse() method added in the ProjectMapper.

--- src/main/java/com/coding/lovable_clone/mapper/ProjectMapper.java
+++ src/main/java/com/coding/lovable_clone/mapper/ProjectMapper.java
@@ -3,6 +3,7 @@ package com.coding.lovable_clone.mapper;
 import com.coding.lovable_clone.dto.project.ProjectResponse;
 import com.coding.lovable_clone.dto.project.ProjectSummaryResponse;
 import com.coding.lovable_clone.entities.Project;
+import com.coding.lovable_clone.enums.ProjectRole;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -16,6 +17,8 @@ public interface ProjectMapper {
 
 //    @Mapping(target = "projectName", source = "name")
 //    ProjectSummaryResponse toProjectSummaryResponse(Project project);
+
+    ProjectSummaryResponse toProjectSummaryResponse(Project project, ProjectRole role);
 
     //List of ProjectEntity to List of ProjectResponse DTO Conversion
     List<ProjectSummaryResponse> toListOfProjectSummaryResponse(List<Project> projects);
