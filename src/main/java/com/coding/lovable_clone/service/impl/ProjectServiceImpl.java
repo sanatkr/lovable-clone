45bb251408185ae06e3362a66a024b0a486576b9
sanat <sanatrathore1214@gmail.com>
2026-03-10 22:09:20 +0530

    getUserProjects() method modified in the ProjectServiceImpl class.

--- src/main/java/com/coding/lovable_clone/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/coding/lovable_clone/service/impl/ProjectServiceImpl.java
@@ -4,18 +4,27 @@ import com.coding.lovable_clone.dto.project.ProjectRequest;
 import com.coding.lovable_clone.dto.project.ProjectResponse;
 import com.coding.lovable_clone.dto.project.ProjectSummaryResponse;
 import com.coding.lovable_clone.entities.Project;
+import com.coding.lovable_clone.entities.ProjectMember;
+import com.coding.lovable_clone.entities.ProjectMemberId;
 import com.coding.lovable_clone.entities.User;
+import com.coding.lovable_clone.enums.ProjectRole;
+import com.coding.lovable_clone.error.BadRequestException;
 import com.coding.lovable_clone.mapper.ProjectMapper;
+import com.coding.lovable_clone.repository.ProjectMemberRepository;
 import com.coding.lovable_clone.repository.ProjectRepository;
 import com.coding.lovable_clone.repository.UserRepository;
+import com.coding.lovable_clone.security.AuthUtil;
 import com.coding.lovable_clone.service.ProjectService;
 
+import com.coding.lovable_clone.service.ProjectTemplateService;
+import com.coding.lovable_clone.service.SubscriptionService;
 import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.util.List;
 
 @Service
@@ -27,11 +36,20 @@ public class ProjectServiceImpl implements ProjectService {
     ProjectRepository projectRepository;
     UserRepository userRepository;
     ProjectMapper projectMapper;
+    ProjectMemberRepository projectMemberRepository;
+    AuthUtil authUtil;
+    SubscriptionService subscriptionService;
+    ProjectTemplateService projectTemplateService;
+
 
     @Override
-    public List<ProjectSummaryResponse> getUserProjects(Long userId) {
-        var projects = projectRepository.findAllAccessibleByUser(userId);
-        return projectMapper.toListOfProjectSummaryResponse(projects);
+    public List<ProjectSummaryResponse> getUserProjects() {
+
+        Long userId = authUtil.getCurrentUserId();
+        var projectsWithRoles = projectRepository.findAllAccessibleByUser(userId);
+        return projectsWithRoles.stream()
+                .map(p -> projectMapper.toProjectSummaryResponse(p.getProject(), p.getRole()))
+                .toList();
 
     }
 
@@ -41,17 +59,39 @@ public class ProjectServiceImpl implements ProjectService {
     }
 
     @Override
-    public ProjectResponse createProject(ProjectRequest request, Long userId) {
+    public ProjectResponse createProject(ProjectRequest request) {
 
-        User owner = userRepository.findById(userId).orElseThrow();
+        if(!subscriptionService.canCreateNewProject()) {
+            throw new BadRequestException("User cannot create a New project with current Plan, Upgrade plan now.");
+        }
+
+        Long userId = authUtil.getCurrentUserId();
+//        User owner = userRepository.findById(userId).orElseThrow(
+//                () -> new ResourceNotFoundException("User", userId.toString())
+//        );
+        User owner = userRepository.getReferenceById(userId);
+
         Project project = Project.builder()
                 .name(request.name())
-                .owner(owner)
                 .isPublic(false)
                 .build();
-
         project = projectRepository.save(project);
+
+        ProjectMemberId projectMemberId = new ProjectMemberId(project.getId(), owner.getId());
+        ProjectMember projectMember = ProjectMember.builder()
+                .id(projectMemberId)
+                .projectRole(ProjectRole.OWNER)
+                .user(owner)
+                .acceptedAt(Instant.now())
+                .invitedAt(Instant.now())
+                .project(project)
+                .build();
+        projectMemberRepository.save(projectMember);
+
+        projectTemplateService.initializeProjectFromTemplate(project.getId());
+
         return projectMapper.toProjectResponse(project);
+
     }
 
     @Override
