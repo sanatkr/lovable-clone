1b665e4fae20d94bb78736bf82336f165344e4bc
sanat <sanatrathore1214@gmail.com>
2026-03-16 21:54:28 +0530

    SubscriptionRepository interface updated-1.

--- src/main/java/com/coding/lovable_clone/repository/SubscriptionRepository.java
+++ src/main/java/com/coding/lovable_clone/repository/SubscriptionRepository.java
@@ -1,7 +1,21 @@
 package com.coding.lovable_clone.repository;
 
 import com.coding.lovable_clone.entities.Subscription;
+import com.coding.lovable_clone.enums.SubscriptionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.Set;
+
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+
+    /*
+     * Get the current active subscription
+     * */
+    Optional<Subscription> findByUserIdAndStatusIn(Long userId, Set<SubscriptionStatus> statusSet);
+
+    boolean existsByStripeSubscriptionId(String subscriptionId);
+
+    Optional<Subscription> findByStripeSubscriptionId(String gatewaySubscriptionId);
+
 }
