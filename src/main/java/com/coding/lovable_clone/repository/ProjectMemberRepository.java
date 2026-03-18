d1033ecb0a245141c7bb06532bbfacc993cfe255
sanat <sanatrathore1214@gmail.com>
2026-03-18 19:33:21 +0530

    ProjectMemberRepository interface updated-1.

--- src/main/java/com/coding/lovable_clone/repository/ProjectMemberRepository.java
+++ src/main/java/com/coding/lovable_clone/repository/ProjectMemberRepository.java
@@ -6,10 +6,12 @@ import com.coding.lovable_clone.enums.ProjectRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, ProjectMemberId> {
 
     List<ProjectMember> findByIdProjectId(Long projectId);
