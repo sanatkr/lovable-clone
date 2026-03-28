95f39077c1ca102751a509e3b808c562983ce4f8
sanat <sanatrathore1214@gmail.com>
2026-03-28 18:40:52 +0530

    SubscriptionServiceImpl class updated with activateSubscription method (updated-2).

--- src/main/java/com/coding/lovable_clone/service/impl/SubscriptionServiceImpl.java
+++ src/main/java/com/coding/lovable_clone/service/impl/SubscriptionServiceImpl.java
@@ -4,8 +4,11 @@ import com.coding.lovable_clone.dto.subscription.CheckoutRequest;
 import com.coding.lovable_clone.dto.subscription.CheckoutResponse;
 import com.coding.lovable_clone.dto.subscription.PortalResponse;
 import com.coding.lovable_clone.dto.subscription.SubscriptionResponse;
+import com.coding.lovable_clone.entities.Plan;
 import com.coding.lovable_clone.entities.Subscription;
+import com.coding.lovable_clone.entities.User;
 import com.coding.lovable_clone.enums.SubscriptionStatus;
+import com.coding.lovable_clone.error.ResourceNotFoundException;
 import com.coding.lovable_clone.mapper.SubscriptionMapper;
 import com.coding.lovable_clone.repository.PlanRepository;
 import com.coding.lovable_clone.repository.ProjectMemberRepository;
@@ -43,6 +46,38 @@ public class SubscriptionServiceImpl implements SubscriptionService {
         );
 
         return subscriptionMapper.toSubscriptionResponse(currentSubscription);
+    }
+
+    @Override
+    public void activateSubscription(Long userId, Long planId, String subscriptionId, String customerId) {
+
+        boolean exists = subscriptionRepository.existsByStripeSubscriptionId(subscriptionId);
+        if (exists) return;
+
+        User user = getUser(userId);
+        Plan plan = getPlan(planId);
+
+        Subscription subscription = Subscription.builder()
+                .user(user)
+                .plan(plan)
+                .stripeSubscriptionId(subscriptionId)
+                .status(SubscriptionStatus.INCOMPLETE)
+                .build();
+
+        subscriptionRepository.save(subscription);
+    }
+
+    ///  Utility methods
+
+    private User getUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User", userId.toString()));
+    }
+
+    private Plan getPlan(Long planId) {
+        return planRepository.findById(planId)
+                .orElseThrow(() -> new ResourceNotFoundException("Plan", planId.toString()));
+
     }
 
 
