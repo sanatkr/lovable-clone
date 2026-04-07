f26c824b963fdcca9665277a697f9e7b1f63f50a
sanat <sanatrathore1214@gmail.com>
2026-04-07 23:47:22 +0530

    SecurityExpressions class created.

---
+++ src/main/java/com/coding/lovable_clone/security/SecurityExpressions.java
@@ -0,0 +1,9 @@
+package com.coding.lovable_clone.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component("security")
+@RequiredArgsConstructor
+public class SecurityExpressions {
+}
