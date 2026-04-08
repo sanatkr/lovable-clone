523df1e0fe2d26373b5417749265e19eca7c2dfd
sanat <sanatrathore1214@gmail.com>
2026-04-08 22:48:36 +0530

    SecurityExpressions class updated with hasPermission() method.

--- src/main/java/com/coding/lovable_clone/security/SecurityExpressions.java
+++ src/main/java/com/coding/lovable_clone/security/SecurityExpressions.java
@@ -1,9 +1,22 @@
 package com.coding.lovable_clone.security;
 
+import com.coding.lovable_clone.enums.ProjectPermission;
+import com.coding.lovable_clone.repository.ProjectMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component("security")
 @RequiredArgsConstructor
 public class SecurityExpressions {
+
+    private final ProjectMemberRepository projectMemberRepository;
+    private final AuthUtil authUtil;
+
+    private boolean hasPermission(Long projectId, ProjectPermission projectPermission) {
+        Long userId = authUtil.getCurrentUserId();
+
+        return projectMemberRepository.findRoleByProjectIdAndUserId(projectId, userId).
+                map(role -> role.getPermissions().contains(projectPermission))
+                .orElse(false);
+    }
 }
