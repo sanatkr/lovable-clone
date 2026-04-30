b554746d7bd3830d9096cb901a7f4ac38c1461ff
sanat <sanatrathore1214@gmail.com>
2026-05-01 00:00:50 +0530

    ChatSessionId entity class created.

---
+++ src/main/java/com/coding/lovable_clone/entities/ChatSessionId.java
@@ -0,0 +1,18 @@
+package com.coding.lovable_clone.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class ChatSessionId implements Serializable {
+
+    Long projectId;
+    Long userId;
+}
