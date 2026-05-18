71eb5469bdcfc9f752b6b69ea5f4ebf4ec9a3a30
sanat <sanatrathore1214@gmail.com>
2026-05-18 21:44:36 +0530

    StripePaymentProcessor class created.

---
+++ src/main/java/com/coding/lovable_clone/service/impl/StripePaymentProcessor.java
@@ -0,0 +1,14 @@
+package com.coding.lovable_clone.service.impl;
+
+import com.coding.lovable_clone.service.PaymentProcessor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class StripePaymentProcessor implements PaymentProcessor {
+
+
+}
